c61170adbb7a25cf7e23e3380a5dddab20e5971b
Wesley <wesley.felix12@hotmail.com>
2022-11-28 19:53:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds')
+const tryAgain = document.querySelector('.tryAgain')
+
+const jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+    mario.classList.remove('jump') 
+    }, 500);
+}
+
+const loop = setInterval(() => {
+    const pipeColision = pipe.offsetLeft;
+    const marioColision = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+    if (pipeColision < 50 && pipeColision > -50 && marioColision < 80) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipeColision}px`
+        
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioColision}px`
+
+        mario.src = "/images/game-over.png"
+        mario.style.width = '40px'
+
+        clouds.style.animationPlayState = "paused"
+
+        tryAgain.style.display = "block"
+
+        clearInterval(loop)
+    }
+
+}, 10)
+
+tryAgain.addEventListener('click', () => {
+    location.reload()
+})
+
+document.addEventListener('keydown', jump);
